841a01895c5b4a010a91925d7d673ea3b9d7216c
news1d <news1dee@yandex.ru>
2025-06-07 19:30:24 +0300

    chore: update CORS setup with whitelist and credentials

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -7,9 +7,19 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get<CoreConfig>(CoreConfig);
+  const allowedOrigins = ['http://localhost:3000', 'https://pickandstore.com'];
 
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
   app.getHttpAdapter().getInstance().set('trust proxy', 1);
   appSetup(app);
   await app.listen(appConfig.port);
